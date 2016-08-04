06754fa56561cc5f2e01eebb67717de499c8367c
Hori <quachhoangtam@gmail.com>
2016-08-04 19:32:04 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var server = require("http").createServer(app);
 var io = require("socket.io").listen(server);
 var fs = require("fs");
 var listusername;
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 5000;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 server.listen(server_ip_address || server_port);
